661456c28a8bc18146651d6ea9d69d992139cab2
RenardBOT <dinoking.36@gmail.com>
2021-01-20 20:00:45 +0100

    XB12

--- TP1/XB12.c
+++ TP1/XB12.c
@@ -8,7 +8,7 @@ UNIQUEMENT AVEC DES OPERATEURS BITWISES
 
 #include <stdio.h>
 
-unsigned int max1(int x)
+int max1(long int x)
 {
     unsigned int res = 0; /* Longueur du plus long mot*/
     unsigned int buff = 0; /* Mémoire */
@@ -30,8 +30,8 @@ unsigned int max1(int x)
 
 int main()
 {
-    unsigned int n = 011011100111110101
-    printf("Longueur de la plus grande suite de 1 du mot donné : %d",max1(n)) //Doit rendre 5
+    long int n = 0111100111110;
+    printf("Longueur de la plus grande suite de 1 du mot donné : %d\n",max1(n)); //Doit rendre 5
 
     return 0;
 }
